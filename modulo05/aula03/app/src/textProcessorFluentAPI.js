ee51ddc3869fbf5e9544b86500c348af9a6138dd
Rodolpho Porto <rodolpho.porto@gmail.com>
2024-01-20 16:50:30 -0300

    Segurança em expressões regulares, Manipulação de erros customizados e Projeto parte 02

---
+++ modulo05/aula03/app/src/textProcessorFluentAPI.js
@@ -0,0 +1,40 @@
+const { evaluateRegex } = require('./util')
+
+// o objetivo do Fluent API é executar tarefas
+// como uma pipeline, step by step
+// e no fim, chama o build. MUITO similar ao padrao Builder
+// a diferença que aqui é sobre processos, o Builder sobre construção
+// de objetos
+class TextProcessorFluentAPI {
+    // propriedade privada!
+    #content
+
+    constructor(content) {
+        this.#content = content
+    }
+    extractPeopleData() {
+        const matchPerson = evaluateRegex(/(?<=[contratante|contratada]:\s{1})(?!\s)(.*\n.*?)$/gmi)
+        // faz o match para encontrar a string inteira que contem os dados que precisamos
+        const onlyPerson = this.#content.match(matchPerson)
+        // console.log('onlyPerson', matchPerson.test(this.#content))
+        this.#content = onlyPerson
+
+        return this
+    }
+    divideTextInColumns() {
+        const splitRegex = evaluateRegex(/,/)
+        this.#content = this.#content.map(line => line.split(splitRegex))
+        return this
+    }
+    removeEmptyCharacters() {
+        const trimSpaces = evaluateRegex(/^\s+|\s+$|\n/g)
+        this.#content = this.#content.map(line => line.map(item => item.replace(trimSpaces, "")))
+
+        return this
+    }
+    build() {
+        return this.#content
+    }
+}
+
+module.exports = TextProcessorFluentAPI
