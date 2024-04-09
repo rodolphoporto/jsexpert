f5b69cf2fb55394c3fa3d9bb0b4a6e8138c22980
Rodolpho Porto <rodolpho.porto@gmail.com>
2024-04-09 15:45:43 -0300

    #2 - Projeto Gerador de código - Criando pastas e arquivos de camadas

--- modulo09/04-codegen/aula02/README.md
+++ modulo09/04-codegen/aula02/README.md
@@ -1,10 +1,10 @@
 # Task Checklist
 
-- [ ] creates 'src' main folder if it not exists
-- [ ] creates 'repository' layer
-- [ ] creates 'service' layer with 'repository' as dependency
-- [ ] creates 'factory' layer with 'service' and 'repository' returning its instances
+- [x] creates 'src' main folder if it not exists
+- [x] creates 'repository' layer
+- [x] creates 'service' layer with 'repository' as dependency
+- [x] creates 'factory' layer with 'service' and 'repository' returning its instances
 - [ ] can create multiples domains with a single command
-- [ ] saves file as 'camelCase' and classes as 'PascalCase'
+- [x] saves file as 'camelCase' and classes as 'PascalCase'
 - [ ] reaches **100% tests coverage**
 - [ ] integration tests should validate files on disk as a valid JS class
--- modulo09/04-codegen/aula02/test/unit/layers.test.js
+++ modulo09/04-codegen/aula02/test/unit/layers.test.js
@@ -6,15 +6,34 @@ import {
     beforeEach
 } from '@jest/globals'
 
-describe('#Util - Strings', () => {
-    
+import { createLayersIfNotExists } from '../../src/createLayers.js'
+import fsPromises from 'fs/promises'
+import fs from 'fs'
+
+describe('#Layers - Folder Structure', () => {
+    const defaultLayers = ['service', 'factory', 'repository']
     beforeEach(() => {
         jest.restoreAllMocks()
         jest.clearAllMocks()
     })
 
-    test('#upperCaseFirstLetter should transform the first letter in uppercase', () => {
-        
+    test('should create folders if it doenst exists', async () => {
+        jest.spyOn(fsPromises, fsPromises.mkdir.name).mockResolvedValue()
+        jest.spyOn(fs, fs.existsSync.name).mockReturnValue(false)
+
+        await createLayersIfNotExists({ mainPath: '', layers: defaultLayers })
+
+        expect(fs.existsSync).toHaveBeenCalledTimes(defaultLayers.length)
+        expect(fsPromises.mkdir).toHaveBeenCalledTimes(defaultLayers.length)
+    })
+    test('should not create folders if it exists', async () => {
+        jest.spyOn(fsPromises, fsPromises.mkdir.name).mockResolvedValue()
+        jest.spyOn(fs, fs.existsSync.name).mockReturnValue(true)
+
+        await createLayersIfNotExists({ mainPath: '', layers: defaultLayers })
+
+        expect(fs.existsSync).toHaveBeenCalledTimes(defaultLayers.length)
+        expect(fsPromises.mkdir).not.toHaveBeenCalled()
     })
 
 })
