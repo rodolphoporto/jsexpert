10929124aaf937f326d171e494e35d69a96247bf
Rodolpho Porto <rodolpho.porto@gmail.com>
2024-04-09 09:01:55 -0300

    #1 - Projeto Gerador de código - Criação de templates e estrutura do projeto

---
+++ modulo09/04-codegen/aula01/src/templates/factoryTemplate.js
@@ -0,0 +1,36 @@
+import Util from "../util.js"
+const serviceNameAnchor = '$$serviceName'
+const repositoryNameAnchor = '$$repositoryName'
+
+const serviceNameDepAnchor = '$$serviceNameDep'
+const repositoryNameDepAnchor = '$$repositoryNameDep'
+
+const componentNameAnchor = '$$componentName'
+
+const template = `
+import $$serviceName from '../service/$$serviceNameDep.js';
+import $$repositoryName from '../service/$$repositoryNameDep.js';
+
+export default class $$componentNameFactory {
+    static getInstance() {
+        const repository = new $$repositoryName();
+        const service = new $$serviceName(repository);
+        return service
+    }
+}`
+
+export function factoryTemplate(componentName, repositoryName, serviceName) {
+    const txtFile = template
+        .replaceAll(componentNameAnchor, Util.upperCaseFirstLetter(componentName))
+
+        .replaceAll(repositoryNameDepAnchor, Util.lowerCaseFirstLetter(serviceName))
+        .replaceAll(repositoryNameDepAnchor, Util.lowerCaseFirstLetter(repositoryName))
+
+        .replaceAll(repositoryNameAnchor, Util.upperCaseFirstLetter(serviceName))
+        .replaceAll(repositoryNameAnchor, Util.upperCaseFirstLetter(repositoryName))        
+
+    return {
+        fileName: `${componentName}Factory`,
+        template: txtFile
+    }
+}
